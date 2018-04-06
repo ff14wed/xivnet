1da04aa8f382231d0ce93debe4e5ce257e553ea5
Flawed <33593723+ff14wed@users.noreply.github.com>
2018-04-05 20:33:17 -0700

    Rename Target to Notify4
    Turns out this opcode was another Actor Control

---
+++ datatypes/notify4.go
@@ -0,0 +1,14 @@
+package datatypes
+
+// Notify4 defines the data array for a notify4 block
+type Notify4 struct {
+	Type     uint16
+	Pad1     uint16
+	P1       uint32
+	P2       uint32
+	P3       uint32
+	P4       uint32
+	Pad2     uint32
+	TargetID uint32 // 0xE0000000 means deselect target
+	U1       uint32
+}
