a00ff7a946217949d93dd0a5d9ca516a811a39cb
Flawed <33593723+ff14wed@users.noreply.github.com>
2018-05-22 11:26:28 -0700

    Updated most opcodes to 4.3

--- datatypes/types.go
+++ datatypes/types.go
@@ -15,41 +15,42 @@ var outTypeRegistry = make(map[uint16]xivnet.BlockData)
 
 // Opcodes that define the datatypes of incoming (from server) network blocks
 const (
-	AddStatusOpcode       = 0x141 // Unchanged for 4.2
-	ActionOpcode          = 0x128 // Updated for 4.2
-	AoEAction8Opcode      = 0x12B // Updated for 4.2
-	AoEAction16Opcode     = 0x138 // Updated for 4.2
-	AoEAction24Opcode     = 0x139 // Updated for 4.2
-	AoEAction32Opcode     = 0x13A // Updated for 4.2
-	CastingOpcode         = 0x162 // Updated for 4.2
-	EquipChangeOpcode     = 0x170 // Updated for 4.2
-	GaugeOpcode           = 0x27D // Updated for 4.2
-	HateListOpcode        = 0x166 // Updated for 4.2
-	HateRankingOpcode     = 0x165 // Updated for 4.2
-	InitZoneOpcode        = 0x19A // Unchanged 4.2
-	MapChangeOpcode       = 0x27C // Updated for 4.2
-	MountOpcode           = 0x1CD // Updated for 4.2
-	MovementOpcode        = 0x15E // Updated for 4.2
-	NotifyOpcode          = 0x142 // Unchanged 4.2
-	Notify3Opcode         = 0x143 // Unchanged 4.2
-	Notify4Opcode         = 0x144 // Unchanged 4.2
-	NPCSpawnOpcode        = 0x15D // Updated for 4.2
-	PerformOpcode         = 0x286 // Updated for 4.2
-	PlayerSpawnOpcode     = 0x15C // Updated for 4.2
-	RemoveEntityOpcode    = 0x191 // Unchanged 4.2
-	SetPosOpcode          = 0x160 // Updated for 4.2
-	UpdateHPMPTPOpcode    = 0x145 // Unchanged 4.2
-	UpdateStatusesOpcode  = 0x125 // Updated 4.2
-	WeatherChangeOpcode   = 0x1EA // Updated 4.2
+	AddStatusOpcode       = 0x141 // Unchanged for 4.3
+	ActionOpcode          = 0x151 // Updated for 4.3
+	AoEAction8Opcode      = 0x154 // Updated for 4.3
+	AoEAction16Opcode     = 0x155 // Updated for 4.3
+	AoEAction24Opcode     = 0x156 // Updated for 4.3
+	AoEAction32Opcode     = 0x157 // Updated for 4.3
+	CastingOpcode         = 0x178 // Updated for 4.3
+	EquipChangeOpcode     = 0x186 // Updated for 4.3
+	GaugeOpcode           = 0x292 // Updated for 4.3
+	HateListOpcode        = 0x17C // Updated for 4.3
+	HateRankingOpcode     = 0x17B // Updated for 4.3
+	InitZoneOpcode        = 0x19A // Unchanged 4.3
+	MapChangeOpcode       = 0x291 // Updated for 4.3
+	MountOpcode           = 0x1E3 // Updated for 4.3
+	MovementOpcode        = 0x174 // Updated for 4.3
+	NotifyOpcode          = 0x142 // Unchanged 4.3
+	Notify3Opcode         = 0x143 // Unchanged 4.3
+	Notify4Opcode         = 0x144 // Unchanged 4.3
+	NPCSpawnOpcode        = 0x173 // Updated for 4.3
+	PerformOpcode         = 0x286 // Updated for 4.3
+	PlayerSpawnOpcode     = 0x172 // Updated for 4.3
+	RemoveEntityOpcode    = 0x191 // Unchanged 4.3
+	SetPosOpcode          = 0x176 // Updated for 4.3
+	UpdateHPMPTPOpcode    = 0x145 // Unchanged 4.3
+	UpdateStatusesOpcode  = 0x14E // Updated for 4.3
+	WeatherChangeOpcode   = 0x200 // Updated for 4.3
 	XWorldPartyListOpcode = 0xA1  // Updated 4.18
 )
 
 var _ = registerInBlockData(AddStatusOpcode, new(AddStatus))
-var _ = registerInBlockData(ActionOpcode, new(Action))
-var _ = registerInBlockData(AoEAction8Opcode, new(AoEAction8))
-var _ = registerInBlockData(AoEAction16Opcode, new(AoEAction16))
-var _ = registerInBlockData(AoEAction24Opcode, new(AoEAction24))
-var _ = registerInBlockData(AoEAction32Opcode, new(AoEAction32))
+
+// var _ = registerInBlockData(ActionOpcode, new(Action))
+// var _ = registerInBlockData(AoEAction8Opcode, new(AoEAction8))
+// var _ = registerInBlockData(AoEAction16Opcode, new(AoEAction16))
+// var _ = registerInBlockData(AoEAction24Opcode, new(AoEAction24))
+// var _ = registerInBlockData(AoEAction32Opcode, new(AoEAction32))
 var _ = registerInBlockData(CastingOpcode, new(Casting))
 var _ = registerInBlockData(EquipChangeOpcode, new(EquipChange))
 var _ = registerInBlockData(GaugeOpcode, new(Gauge))
@@ -75,9 +76,9 @@ var _ = registerInBlockData(WeatherChangeOpcode, new(WeatherChange))
 
 // Opcodes that define the datatypes of outgoing (to server) network blocks
 const (
-	MyActionOpcode    = 0x131 // Updated for 4.18
-	MyMovementOpcode  = 0x138 // Updated for 4.2
-	MyMovement2Opcode = 0x177 // Updated for 4.2
+	MyActionOpcode    = 0x13D // Updated for 4.3
+	MyMovementOpcode  = 0x144 // Updated for 4.3
+	MyMovement2Opcode = 0x183 // Updated for 4.3
 	MyPerformOpcode   = 0x180 // Updated for 4.18
 )
 
