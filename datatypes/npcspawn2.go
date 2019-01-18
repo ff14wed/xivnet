7babc65de0892d758fab37288a5bf9695ae8730c
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-01-18 08:39:22 -0800

    Add NPCSpawn2 data type

---
+++ datatypes/npcspawn2.go
@@ -0,0 +1,12 @@
+package datatypes
+
+// NPCSpawn2 defines the data array for a new entity block
+// Notes:
+// GMRank from PlayerSpawn corresponds to MobAggression in
+// this struct
+// This packet type is encountered in the wild when spawning Alliance Raid
+// bosses
+type NPCSpawn2 struct {
+	PlayerSpawn
+	U31 [47]uint64
+}
