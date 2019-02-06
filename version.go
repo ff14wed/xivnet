a0077993338060b9e2ffd44d494416035aadabe4
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-02-06 08:09:08 -0800

    Add version file to tie xivnet to FFXIV version

---
+++ version.go
@@ -0,0 +1,7 @@
+package xivnet
+
+// FFXIV_PATCH_VERSION defines the exact patch version of FFXIV that this
+// library supports. We tie xivnet to this version since the network opcodes
+// usually only change on minor patch version updates and not smaller updates
+// like hotfixes.
+const FFXIV_PATCH_VERSION = "4.5"
