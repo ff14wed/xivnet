ebe1cff8851f1f0804790757de368c2d6b478de3
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-06-28 05:16:42 -0700

    Fixed Egress Movement packets for 5.0

--- datatypes/egressinstancemovement.go
+++ datatypes/egressinstancemovement.go
@@ -3,10 +3,11 @@ package datatypes
 // EgressInstanceMovement defines the data array for a outgoing movement block inside an instance
 type EgressInstanceMovement struct {
 	Direction float32 // 0 is South. Range [-pi,pi] <=> Counterclockwise from North
-	U1        [6]uint32
+	U1        [5]uint32
 	X         float32
 	Y         float32
 	Z         float32
+	U2        uint32
 }
 
 func (EgressInstanceMovement) IsBlockData() {}
--- datatypes/egressmovement.go
+++ datatypes/egressmovement.go
@@ -4,10 +4,10 @@ package datatypes
 type EgressMovement struct {
 	Direction float32 // 0 is South. Range [-pi,pi] <=> Counterclockwise from North
 	U1        uint32
-	U2        uint32
 	X         float32
 	Y         float32
 	Z         float32
+	U2        uint32
 }
 
 func (EgressMovement) IsBlockData() {}
