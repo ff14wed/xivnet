b27a2cf314ac08976d19b03830af91f62d731811
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-07-14 18:09:41 -0700

    [Breaking Change] Change StatusEffect's Extra -> Param

--- datatypes/statuseffect.go
+++ datatypes/statuseffect.go
@@ -3,7 +3,7 @@ package datatypes
 // StatusEffect represents the data structure for a status effect
 type StatusEffect struct {
 	ID       uint16
-	Extra    uint16
+	Param    uint16
 	Duration float32
 	ActorID  uint32
 }
