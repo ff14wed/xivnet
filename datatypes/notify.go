635d0d7d58aed4983e50c256af53526f0ad200bd
Flawed <33593723+ff14wed@users.noreply.github.com>
2019-07-28 04:19:09 -0700

    Condense similar types into the same file

--- datatypes/notify.go
+++ datatypes/notify.go
@@ -31,3 +31,33 @@ Type:39 => Remove Entity ???
 */
 
 func (Notify142) IsBlockData() {}
+
+// Notify143 defines the data array for a notify3 block
+type Notify143 struct {
+	Type uint16
+	Pad1 uint16
+	P1   uint32
+	P2   uint32
+	P3   uint32
+	P4   uint32
+	P5   uint32
+	P6   uint32
+	Pad2 uint32
+}
+
+func (Notify143) IsBlockData() {}
+
+// Notify144 defines the data array for a notify4 block
+type Notify144 struct {
+	Type     uint16
+	Pad1     uint16
+	P1       uint32
+	P2       uint32
+	P3       uint32
+	P4       uint32
+	Pad2     uint32
+	TargetID uint32 // 0xE0000000 means deselect target
+	U1       uint32
+}
+
+func (Notify144) IsBlockData() {}
--- datatypes/spawn.go
+++ datatypes/spawn.go
@@ -122,3 +122,27 @@ type PlayerSpawn struct {
 }
 
 func (PlayerSpawn) IsBlockData() {}
+
+// NPCSpawn defines the data array for a new entity block
+// Notes:
+// GMRank from PlayerSpawn corresponds to MobAggression in
+// this struct
+type NPCSpawn struct {
+	PlayerSpawn
+	U31 uint64
+}
+
+func (NPCSpawn) IsBlockData() {}
+
+// NPCSpawn2 defines the data array for a new entity block
+// Notes:
+// GMRank from PlayerSpawn corresponds to MobAggression in
+// this struct
+// This packet type is encountered in the wild when spawning Alliance Raid
+// bosses
+type NPCSpawn2 struct {
+	PlayerSpawn
+	U31 [47]uint64
+}
+
+func (NPCSpawn2) IsBlockData() {}
